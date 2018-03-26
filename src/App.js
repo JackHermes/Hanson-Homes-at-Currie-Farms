0de25432bd2f0ee340c08ec0d00510c61b5ba29a
Jordan Scholten <schojo13@gmail.com>
2018-03-26 15:44:58 -0700

    Add contact info and 'under construction' message

--- src/App.js
+++ src/App.js
@@ -10,10 +10,28 @@ class App extends Component {
       <div className="App">
         <Header />
         <p className="App-intro">
-      
+
         </p>
         <div className="App-body">
+          <h2>Site currently under construction. Please check back later!</h2>
           <img src={drawnHome} alt="Drawn home" />
+          <div class="contacts">
+            <h3 class="bold">Listing Agent:</h3>
+            <div>Steve Larsen</div>
+            <div>(206)799-6300</div>
+            <div>slarsen@windermere.com</div>
+          </div>
+          <br/>
+          <div>
+            <h3 class="bold">Buyer's Agents:</h3>
+            <div>Shirley Hudson</div>
+            <div>(425)923-4593</div>
+            <div>shirleyhudson@windermere.com</div>
+            <br/>
+            <div>Marlene Rouleau</div>
+            <div>(425)409-6041</div>
+            <div>marlener@windermere.com</div>
+          </div>
         </div>
         <Footer />
       </div>
--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import LLCLogo from './images/currieFarmsLLC.jpg';
+import CFLogo from './images/currieFarmsLLC.jpg';
+import WindLogo from './images/WindermereLogo.png';
 
 class Footer extends Component {
   render() {
     return (
       <footer className="App-footer">
-        <img src={LLCLogo} alt="Hanson Homes at Currie Farms LLC" />
+        <img src={CFLogo} alt="Hanson Homes at Currie Farms LLC" />
+        <img src={WindLogo} alt="Windermere Real Estate" />
       </footer>
     );
   }
